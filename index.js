a8923cffb4505b6b193b4ca4fe86f3fcebc9c7f1
lferreira <lferreira@atcan.es>
2016-02-19 14:56:09 +0000

    Adding login and writing text.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var app = require('express')();
+var express = require('express');
+
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/styles', express.static(__dirname + '/views/styles/'));
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function(socket) {
 	//console.log('a user connected');
-	//socket.on('disconnect', function() {
-	//	console.log('user disconnected');
-	//});
+	socket.on('disconnect', function(msg) {
+		io.emit('disconnected', msg);
+	});
 	socket.on('chat message', function(msg) {
 		//console.log('message: ' + msg);
 		io.emit('chat message', msg);
+	});
+	socket.on('login', function(msg) {
+		io.emit('connected', msg);
 	});
 });
 
